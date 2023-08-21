655eddb454cc78d1d9fde5a8fba177fd76fa6a4e
javierh0815 <javier.hurtado0815@gmail.com>
2023-08-21 19:13:22 -0400

    cambio en capacitor config ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'cl.amahur.app',
   appName: 'registrapp',
   webDir: 'www',
   server: {
