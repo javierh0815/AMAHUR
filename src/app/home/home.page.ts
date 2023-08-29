7570d5627932a38a3c365bd8c08fd8d533657494
CosmisFlake <jo.amaya@duocuc.cl>
2023-08-28 20:55:52 -0400

    Animacion de Botones Home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import type { Animation } from '@ionic/angular';
 import { Home } from 'src/app/models/home';
+import { AnimationController, IonCard } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -9,9 +11,14 @@ import { Home } from 'src/app/models/home';
 })
 export class HomePage implements OnInit {
 
+  @ViewChild(IonCard, { read: ElementRef })
+  card!: ElementRef<HTMLIonCardElement>;
+
+  private animation!: Animation;
+
   homeArray:Home[]=[];
 
-  constructor(private router:Router) {}
+  constructor(private router:Router, private animationCtrl: AnimationController) {}
   
   ngOnInit() {
     this.menuHome();
@@ -35,6 +42,30 @@ export class HomePage implements OnInit {
   }
     )
 
+  }
+
+  ngAfterViewInit(){
+    this.animation =  this.animationCtrl.
+    create()
+    .addElement(document.querySelectorAll("ion-card .array-boton"))
+    .duration(1000)
+    .iterations(Infinity)
+    .direction('alternate')
+    .fromTo('background','#AC8BEE','var(--ion-item-background)');
+
+
+  }
+
+  play() {
+    this.animation.play();
+  }
+
+  pause() {
+    this.animation.pause();
+  }
+
+  stop() {
+    this.animation.stop();
   }
 
   
