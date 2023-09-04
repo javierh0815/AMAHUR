6536db8226e5c3522efdb462e3556eb56d2dcc6e
CosmisFlake <jo.amaya@duocuc.cl>
2023-09-04 19:18:35 -0300

    Rediseño del menu, skeleton box y animaciones en el fondo

--- src/app/models/home.ts
+++ src/app/models/home.ts
@@ -1,6 +1,15 @@
+export interface Menu{
+    id:number;
+    nombre:string;
+    url:string;
+    icono:string;
+    disabled?:boolean;
+}
+
 export interface Home{
     id:number;
     nombre:string;
     url:string;
     icono:string;
+    disabled?:boolean;
 }
