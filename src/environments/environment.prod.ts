4b0e9c254bd2d106cc976d5aedc18273cc9acf47
Javier Hurtado <javier.hurtado0815@gmail.com>
2023-09-27 16:13:41 -0300

    agregando firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+
+    apiKey: "AIzaSyB9PJ6KQ_YdUx_PZxpYLjNRNCi3mBPUzmI",
+  
+    authDomain: "amahur.firebaseapp.com",
+  
+    projectId: "amahur",
+  
+    storageBucket: "amahur.appspot.com",
+  
+    messagingSenderId: "275162227049",
+  
+    appId: "1:275162227049:web:c6dfd7ece8a13ec7fb1df8"
+  
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,22 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+
+    apiKey: "AIzaSyB9PJ6KQ_YdUx_PZxpYLjNRNCi3mBPUzmI",
+  
+    authDomain: "amahur.firebaseapp.com",
+  
+    projectId: "amahur",
+  
+    storageBucket: "amahur.appspot.com",
+  
+    messagingSenderId: "275162227049",
+  
+    appId: "1:275162227049:web:c6dfd7ece8a13ec7fb1df8"
+  
+  }
 };
 
 /*
