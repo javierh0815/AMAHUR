149f1e34dedbdd0fda875fc7a2f2d7761f5b0ca3
Javier Hurtado <javier.hurtado0815@gmail.com>
2023-10-18 10:40:44 -0300

    avance registro de usuario, aun pendiente de terminar

--- src/app/pages/asistencia/asistencia.page.ts
+++ src/app/pages/asistencia/asistencia.page.ts
@@ -4,6 +4,7 @@ import { HelperService } from 'src/app/services/helper.service';
 import { DetalleAsignaturaPage } from '../detalle-asignatura/detalle-asignatura.page';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { ModalController } from '@ionic/angular';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-asistencia',
@@ -15,12 +16,23 @@ export class AsistenciaPage implements OnInit {
   @Input() datosMalla:any[]=[];
 
   seleccionAsignatura:string='';
+  docente:string='';
+  fecha:string='';
+  hora:string='';
+  leccion:string='';
+  sala:string='';
+  seccion:string='';
+  correoA:string='';
+  correoEstudiante:string='';
+  usuarioA:any;
+  
   
 
   constructor(private router:Router,
               private helper:HelperService,
               private auth:AngularFireAuth,
-              private modalC:ModalController) { }
+              private modalC:ModalController,
+              private storage:StorageService) { }
 
   ngOnInit() {
     console.log("malla: ",this.datosMalla);
@@ -40,12 +52,32 @@ export class AsistenciaPage implements OnInit {
     if(confirm == true) {
       const loader = await this.helper.showLoader("Cargando...");
       const seleccionA = this.datosMalla.find(asignatura => asignatura.asignatura === this.seleccionAsignatura);
-      
+      this.usuarioA = await this.storage.obtenerUser();
+      const estudianteToken = await this.auth.currentUser;
+      const usuarioF = this.usuarioA.find((e: {correo:string; }) => e.correo == estudianteToken?.email);
+      this.correoEstudiante = usuarioF.correo;
+
       if(seleccionA){
         await loader.dismiss();
         const nomA = seleccionA.leccion;
-        console.log("a",nomA);
+        
+        console.log("a",nomA,"c",this.correoEstudiante);
       }
+
+      
+      var presencia =  [{
+        asignatura:seleccionA.leccion,
+        docente:seleccionA?.docente,
+        fecha:seleccionA?.fecha,
+        hora:seleccionA?.hora,
+        leccion:seleccionA?.leccion,
+        sala:seleccionA?.sala,
+        seccion:seleccionA?.seccion,
+        correo:this.correoEstudiante
+
+      }]
+      console.log(presencia);
+      
       
       //this.helper.showModal(DetalleAsignaturaPage);
     }
