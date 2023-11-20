ee825706b834334caa8863b0a8f64e6ba92d3113
Javier Hurtado <javier.hurtado0815@gmail.com>
2023-11-20 19:54:31 -0300

    avance de pruebas, pendiente perfil usuario

--- src/app/pages/recuperar/recuperar.page.spec.ts
+++ src/app/pages/recuperar/recuperar.page.spec.ts
@@ -1,17 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RecuperarPage } from './recuperar.page';
+import { AngularDelegate, IonicModule, ModalController } from '@ionic/angular';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment';
 
 describe('RecuperarPage', () => {
   let component: RecuperarPage;
   let fixture: ComponentFixture<RecuperarPage>;
 
   beforeEach(() => {
+
+    TestBed.configureTestingModule({
+      declarations:[RecuperarPage],
+      imports: [IonicModule.forRoot(),        AngularFireModule.initializeApp(environment.firebaseConfig)],
+      providers:[ModalController,AngularDelegate]
+    })
     fixture = TestBed.createComponent(RecuperarPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  /* it('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
-  }); */
+  });
 });
